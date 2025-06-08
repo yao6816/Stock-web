85123ba85829344cec63e0ab9b88ff60831958fe
yerik <jing.yao6816@gmail.com>
2025-06-08 17:29:40 +0800

    Add Script

---
+++ front/script.js
@@ -0,0 +1,17 @@
+// Stock data array
+let stocks = [];
+
+// APi key for Alpha Vantage (IMPORTANT: This is a demo key with limitations)
+//For a real application, use your own key and consider a backend proxy to protect it.
+const API_KEY = '';
+
+let notificationTimeout;
+
+// Function to show notifications
+fun showNotification(message, type = 'info', duratiion = 5000){
+    const notification = document.getElementById('notification');
+    const notificationMessage = document.getElementById('notification-message');
+    const closeBtn = document.getElementById('close-notification');
+    
+    if (!notification = )
+}
